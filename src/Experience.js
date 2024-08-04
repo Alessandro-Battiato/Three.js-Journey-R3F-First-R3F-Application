a9b0b4a549f688ef2fcd483839dbe072065d4df2
alessandrobattiato <alessandroDev@outlook.it>
2024-08-04 12:24:36 +0200

    feat: render scene

--- src/Experience.js
+++ src/Experience.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { DoubleSide } from "three";
 
 const Experience = () => {
     return (
         <>
-            <mesh>
-                <sphereGeometry args={[1.5, 32, 32]} />
-                <meshBasicMaterial />
+            <mesh position-x={3} rotation-y={Math.PI * 0.23} scale={1.5}>
+                {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+                <boxGeometry />
+                <meshBasicMaterial color="mediumPurple" />
+            </mesh>
+            <mesh position-x={-3}>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
+            <mesh scale={10} position-y={-1} rotation-x={Math.PI * 0.5}>
+                <planeGeometry />
+                <meshBasicMaterial side={DoubleSide} color="greenyellow" />
             </mesh>
         </>
     );
