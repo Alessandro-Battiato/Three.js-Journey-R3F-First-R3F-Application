6b283d1fff296bb23d5f8b541c3792aa2dd111a0
alessandrobattiato <alessandroDev@outlook.it>
2024-08-04 18:00:14 +0200

    perf: store positions value inside a usememo

--- src/CustomObject.js
+++ src/CustomObject.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { DoubleSide } from "three";
 
 const CustomObject = () => {
     const verticesCount = 10 * 3; // 10 triangles composed of 3 vertices
-    const positions = new Float32Array(verticesCount * 3); // each single vertex is composed of x y z
 
-    for (let i = 0; i < verticesCount * 3; i++) {
-        positions[i] = (Math.random() - 0.5) * 3;
-    }
+    const positions = useMemo(() => {
+        const positions = new Float32Array(verticesCount * 3); // each single vertex is composed of x y z
+
+        for (let i = 0; i < verticesCount * 3; i++) {
+            positions[i] = (Math.random() - 0.5) * 3;
+        }
+
+        return positions;
+    }, []);
 
     return (
         <mesh>
