0dc6de0d1324a4cb0078ef2541cf2c3c002bb8f7
alessandrobattiato <alessandroDev@outlook.it>
2024-08-04 12:51:10 +0200

    feat: enable orbit controls

--- src/Experience.js
+++ src/Experience.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { DoubleSide } from "three";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, extend, useThree } from "@react-three/fiber";
 import { useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+extend({ OrbitControls }); // extend is used to convert a non-native R3F class into a native one
 
 const Experience = () => {
     const cubeRef = useRef(null);
     const groupRef = useRef(null);
+    const { camera, gl } = useThree(); // this hook provides the state (from which you are destructuring camera and gl) that you can see as a PARAMETER of the useFrame, but it will be provided only ONCE instead of giving it to us on each frame
 
     useFrame((state, delta) => {
         // This will be called each frame BEFORE rendering the scene
@@ -18,6 +22,8 @@ const Experience = () => {
 
     return (
         <>
+            <orbitControls args={[camera, gl.domElement]} />
+            {/* this is official THREE.JS documentation, you need to provide these elements to the orbit controls */}
             <group ref={groupRef}>
                 <mesh
                     ref={cubeRef}
