4b937ef00be817a736fb4785a50e876d9bf54871
alessandrobattiato <alessandroDev@outlook.it>
2024-08-04 12:33:51 +0200

    feat: implement animation

--- src/Experience.js
+++ src/Experience.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { DoubleSide } from "three";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const Experience = () => {
+    const cubeRef = useRef(null);
+
+    useFrame((state, delta) => {
+        // This will be called each frame BEFORE rendering the scene
+        // To animate objects, everything has to be done inside this hook, otherwise if you used useState or something like that
+        // you would trigger re renders for each frame and this would be horrible for performances
+
+        cubeRef.current.rotation.y += delta; // using delta the animation will be the same for everyone even for the most powerful gaming pcs
+    });
+
     return (
         <>
-            <mesh position-x={3} rotation-y={Math.PI * 0.23} scale={1.5}>
+            <mesh
+                ref={cubeRef}
+                position-x={3}
+                rotation-y={Math.PI * 0.23}
+                scale={1.5}
+            >
                 {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
                 <boxGeometry />
                 <meshBasicMaterial color="mediumPurple" />
